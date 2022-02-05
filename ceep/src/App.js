e1444003799931e55c2ec846e5da78868d2e4eea
Matheus <matheus@tdzain.com>
2022-02-05 10:20:41 -0300

    Entendendo a lib

---
+++ ceep/src/App.js
@@ -0,0 +1,15 @@
+import ListadeNotas from "./components/ListadeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+
+
+function App() {
+  return (
+    <>
+
+      <FormularioCadastro />
+      <ListadeNotas />
+    </>
+  );
+}
+
+export default App;
